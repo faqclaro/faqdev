9ed2b9cb96d5f96a565a20bd1f9f8bacfa1830e2
Michel Claro <michel.oliveira@orys.com.br>
2024-06-13 14:59:39 -0300

    Títulos em links e Links externos

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -6,12 +6,12 @@ const Breadcrumb = ({ items }) => {
     <nav className="mdn-Breadcrumb">
       <ul className="mdn-Breadcrumb-list">
         <li className="mdn-Breadcrumb-list-item">
-          <a href="/faq/" className="mdn-Breadcrumb-link">Home</a>
+          <a href="/faq/" title='Home' className="mdn-Breadcrumb-link">Home</a>
           <i className="mdn-Breadcrumb-icon mdn-Icon-direita mdn-Icon--md" aria-hidden="true"></i>
         </li>
         {items.map((item, index) => (
           <li key={index} className="mdn-Breadcrumb-list-item">
-            <a href={item.url} className="mdn-Breadcrumb-link">{item.name}</a>
+            <a href={item.url} title={item.name} className="mdn-Breadcrumb-link">{item.name}</a>
             {index < items.length - 1 && (
               <i className="mdn-Breadcrumb-icon mdn-Icon-direita mdn-Icon--md" aria-hidden="true"></i>
             )}
--- components/Footer.js
+++ components/Footer.js
@@ -10,39 +10,53 @@ export default function Sample() {
           social: [
             {
               href: 'https://www.facebook.com/clarobrasil',
-              icon: 'facebook'
+              icon: 'facebook',
+              title: 'Facebook',
+              rel: 'nofollow'
             },
             {
               href: 'https://twitter.com/clarobrasil',
-              icon: 'twitter'
+              icon: 'twitter',
+              title: 'Twitter',
+              rel: 'nofollow'
             },
             {
               href: 'https://www.instagram.com/clarobrasil/',
-              icon: 'instagram'
+              icon: 'instagram',
+              title: 'Instagram',
+              rel: 'nofollow'
             },                        
             {
               href: 'https://www.youtube.com/user/ClaroBrasil',
-              icon: 'youtube'
+              icon: 'youtube',
+              title: 'Youtube',
+              rel: 'nofollow'
             },
             {
               href: 'https://www.linkedin.com/company/clarobrasil',
-              icon: 'linkedin'
+              icon: 'linkedin',
+              title: 'Linkedin',
+              rel: 'nofollow'
             },
             {
               href: 'https://www.tiktok.com/@clarobrasil',
-              icon: 'tik-tok'
+              icon: 'tik-tok',
+              title: 'Tik-tok',
+              rel: 'nofollow'
             },
           ],
           privacy: [
             {
               href: 'https://www.claro.com.br/privacidade/politica-de-privacidade',
               target: '_blank',
-              text: 'Política de Privacidade'
+              text: 'Política de Privacidade',
+              title: 'Política de Privacidade'
             },
             {
               href: 'https://www.claro.com.br/privacidade',
               target: '_blank',
-              text: 'Portal de Privacidade'
+              text: 'Portal de Privacidade',
+              title: 'Portal de Privacidade'
             }
           ]
         }}
